211db6b840da966c7d6b27c551d36f5153717854
Shreyashi Shree <shshreyashi2501@gmail.com>
2026-05-16 08:37:00 +0000

    DevSecOps pipeline added

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+ENV = os.getenv("ENV", "dev")
+
 @app.route("/")
 def home():
-    return "DevSecOps Pipeline Running"
+    return f"DevSecOps Pipeline Running - {ENV.upper()} Environment"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
