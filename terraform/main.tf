7648c7d3fc42d54ee5f550bd6f8ece1042123015
Shreyashi Shree <shshreyashi2501@gmail.com>
2026-05-16 06:42:16 +0000

    Added Terraform IaC scanning

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "my-public-bucket"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
